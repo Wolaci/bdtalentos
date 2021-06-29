42d22a4f2f36e13e5b246a7a8546f73da47138fd
Wolaci <wolacijose8@gmail.com>
2021-06-29 17:41:01 -0300

    Criando cadastro de vagas

---
+++ app/Entity/Vaga.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity;
+
+use \App\Db\Database;
+
+class Vaga{
+
+    public $id;
+    public $titulo;
+    public $descricao;
+    public $ativo;
+    public $data;
+
+    public function cadastrar(){
+        $this->data = date('Y-m-d H:i:s');
+        $obDatabase = new Database('vagas');
+        $this->id = $obDatabase->insert([
+                                            'titulo' => $this->titulo,
+                                            'descricao' => $this->descricao,
+                                            'ativo' => $this->ativo,
+                                            'data' => $this->data
+                                        ]);
+        var_dump($this);                                
+    }
+
+}
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Banco de talentos</title>
+  </head>
+  <body class="bg-dark text-light">
+
+    <div class="container">
+
+      <div class="jumbotrom bg-danger">
+        <h1>Banco de talentos</h1>
+        <p>Vagas</p>
+      </div>
