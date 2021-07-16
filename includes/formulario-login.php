06d8f2ae1b755d647ceaba5c628bc62e9e82c60a
Wolaci <wolacijose8@gmail.com>
2021-07-16 14:57:01 -0300

    Criando formulário de registro

--- includes/formulario-login.php
+++ includes/formulario-login.php
@@ -20,7 +20,27 @@
     </div>
 
     <div class="col">
+      <form method="post">
+        <h2>Cadastre-se</h2>
         
+        <div class="form-group">
+          <label for="">Nome</label>
+          <input type="text" placeholder="Nome" name="nome" class="form-control" required>
+        </div>
+        <div class="form-group">
+          <label for="">E-mail</label>
+          <input type="email" placeholder="E-mail" name="email" class="form-control" required>
+        </div>
+
+        <div class="form-group">
+          <label for="">Senha</label>
+          <input type="password" placeholder="Senha" name="senha" class="form-control" required>
+        </div>
+
+        <div class="form-group">
+          <button type="submit" name="acao" value="cadastrar" class="btn btn-primary">Cadastrar</button>
+        </div>
+      </form>
     </div>
   </div>
 </div>
