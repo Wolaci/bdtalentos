07cc034c56e1d74549f56968374ed7f069fe8721
Wolaci <wolacijose8@gmail.com>
2021-07-16 14:49:53 -0300

    Criando formulário de login

---
+++ includes/formulario-login.php
@@ -0,0 +1,26 @@
+<div class="jumbotrom text-dark">
+  <div class="row">
+    <div class="col">
+      <form method="post">
+        <h2>Login</h2>
+        <div class="form-group">
+          <label for="">E-mail</label>
+          <input type="email" placeholder="E-mail" name="email" class="form-control" required>
+        </div>
+
+        <div class="form-group">
+          <label for="">Senha</label>
+          <input type="password" placeholder="Senha" name="senha" class="form-control" required>
+        </div>
+
+        <div class="form-group">
+          <button type="submit" name="acao" value="logar" class="btn btn-primary">Entrar</button>
+        </div>
+      </form>
+    </div>
+
+    <div class="col">
+        
+    </div>
+  </div>
+</div>
