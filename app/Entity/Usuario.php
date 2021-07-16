17905525c0c0091b789054bf0e971c166876278a
Wolaci <wolacijose8@gmail.com>
2021-07-16 15:10:30 -0300

    Criando classe Usuarip

---
+++ app/Entity/Usuario.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity;
+
+use \App\Db\Database;
+use \PDO;
+
+class Usuario{
+  
+  public $id;
+  public $nome;
+  public $email;
+  public $senha;
+
+  public function cadastrar(){
+    $obDatabase = new Database('usuarios');
+    
+    $this->id = $obDatabase->insert([
+      'nome' => $this->nome,
+      'email' => $this->email,
+      'senha' => $this->senha
+    ]);
+
+    return true;
+  }
+
+}
