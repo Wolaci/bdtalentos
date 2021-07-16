dd5fb1fd1248abee442d4035adb25e7a6ec9c11c
Wolaci <wolacijose8@gmail.com>
2021-07-16 15:27:17 -0300

    Concluindo cadastro de usuários na base de dados

--- login.php
+++ login.php
@@ -1,9 +1,32 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 
+use \App\Entity\Usuario;
 use \App\Session\Login;
 
 Login::requireLogout();
+
+if(isset($_POST['acao'])){
+
+  switch($_POST['acao']){
+
+    case 'logar':
+      break;
+
+    case 'cadastrar':
+      if(isset($_POST['nome'], $_POST['email'], $_POST['senha'])){
+
+        $obUsuario = new Usuario();
+        $obUsuario->nome = $_POST['nome'];
+        $obUsuario->email = $_POST['email'];
+        $obUsuario->senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+        $obUsuario->cadastrar();
+
+      }
+      break;
+  }
+
+}
 
 include __DIR__.'/includes/header.php';
 include __DIR__.'/includes/formulario-login.php';
